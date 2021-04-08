4976dcac0132628ca7300c3126ba9e861c76f5cd
Curtis Maves <curtis@maves.io>
2021-04-08 19:05:28 -0400

    Add CI workflow (#2)
    * Add linting and formatting CI checks
    
    * Update action.yaml
    
    * Added unit testing action
    
    * Update CI workflow
    
    * Move action files

---
+++ .github/actions/ci/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if ! [ -e /var/run/dbus/system_bus_socket ]; then
+    echo Dbus Daemon not found, starting...
+    mkdir -p /var/run/dbus
+    dbus-daemon --system --fork
+fi
+
+dbus-run-session --config-file=tests/session.conf -- sh -c 'echo $DBUS_SESSION_BUS_ADDRESS
+dbus-test-tool echo --sleep-ms=1000 --name=io.maves.LongWait &
+dbus-test-tool echo --sleep-ms=50 --name=io.maves.ShortWait & 
+dbus-test-tool echo --sleep-ms=0 --name=io.maves.NoWait & 
+cargo test && cargo test -- --ignored'
+
